e93d1fbedd312ff6af697866a16b1bcba38e448e
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-02-12 19:46:42 +0200

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import logo from '../assets/logos/logo.png';
+
+export default function Header() {
+    return <header>
+        <img src={logo} ></img>
+        <h1>Office spaces app</h1>
+    </header>
+}
